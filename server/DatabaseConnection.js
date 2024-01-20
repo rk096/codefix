3a0d8ba26701872ceb6c3f9af3e5119ab2905f46
Dhruvin Kavathiya <dhruvink79@gmail.com>
2024-01-20 18:22:02 +0530

    Update DatabaseConnection.js (URI leak)

--- server/DatabaseConnection.js
+++ server/DatabaseConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 
-const url = "mongodb+srv://dhruvink79:xe0oUEM4cNBUp7rG@dev-cluster.eg0e1f8.mongodb.net/codehub?retryWrites=true&w=majority";
+const url = <your mongo db uri>
 
 module.exports.connect = () => {
     mongoose.connect(url)
